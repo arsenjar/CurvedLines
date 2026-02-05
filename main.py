a42967187a33c5516e7d6b53a8f6bd12ecac7c77
arsenjar <pl260302@ahschool.com>
2026-02-05 07:43:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,238 +1,133 @@
 import cv2
 import numpy as np
+from scipy.ndimage import uniform_filter1d
 
-class LineDetector:
+# install opencv-contrib
+
+class Curved_lines_detector:
     def __init__(self):
         pass
 
-    def reseize(self, image):
-        return_img_resized = cv2.resize(image, (640, 480), interpolation=cv2.INTER_AREA)
-        return return_img_resized
+    # getting binary mask for the lines
+    def threshold_img(self, image):
+        # getting image as an input
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower_blue = np.array([90, 10, 150])
+        upper_blue = np.array([140, 255, 255])
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        return mask #binary mask
 
-    def region_of_interest(self, frame):
-        x, y, w, h = (0, 180, 640, 300)
-        if w > 0 and h > 0:
-            # Crop image
-            cropped_image = frame[y:y + h, x:x + w].copy()
+    def frame_preprocessing(self, frame):
+        # resizing the image
+        resized = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_AREA)
+        #blur
+        resized_blured = cv2.blur(resized, (9, 9))
+        resized_blured = cv2.GaussianBlur(resized_blured, (11, 11), 0)
+        black_and_white_mask = self.threshold_img(resized_blured)
 
-        return cropped_image, y
+        # morphology
+        kernel = np.ones((5, 5), np.uint8)
+        black_and_white_mask = cv2.morphologyEx(black_and_white_mask, cv2.MORPH_CLOSE, kernel, iterations=2)
+        black_and_white_mask = cv2.morphologyEx(black_and_white_mask, cv2.MORPH_OPEN, kernel, iterations=1)
 
-    # def threshold_img(self, image):
-    #     #preprocess
-    #     bgr_img  = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #convert image into bgr
-    #     blur_img = cv2.GaussianBlur(bgr_img, (7,7), 0)
-    #
-    #     #thresh
-    #     _, thresh = cv2.threshold(blur_img, 127, 255, cv2.THRESH_BINARY)
-    #
-    #     return thresh
+        #detection
+        skeleton = cv2.ximgproc.thinning(black_and_white_mask)
 
-    def normalization_gray(self, img):
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        num2, labels2, stats2, _ = cv2.connectedComponentsWithStats((skeleton > 0).astype(np.uint8), connectivity=8)
+        # skeleton > 0 - True line pixel
+        # num2 - number of connected components
+        # labels2 - same size matrix
+        # stats2 - component statistics
+        # Getting 2 big components
+        areas = stats2[1:, cv2.CC_STAT_AREA]
+        order = np.argsort(areas)[::-1]  #sorting down
+        top = order[:2] + 1  # we exclude background
 
-        clahe = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(16,16))
+        line_clusters = []
+        for lab in top:
+            ys, xs = np.where(labels2 == lab)
+            pts = np.array(list(zip(xs, ys)), dtype=np.int32) # optimized array
+            line_clusters.append(pts)
 
-        norm = clahe.apply(gray)
+        colors = [(255, 0, 0), (0, 255, 0)]  # BGR
+        for i, cluster in enumerate(line_clusters):
+            for x, y in cluster:
+                cv2.circle(resized, (int(x), int(y)), 2, colors[i], -1)
+
+        # mean 1 line
+        mean_x0 = float(np.mean(line_clusters[0][:, 0]))
+        mean_y0 = float(np.mean(line_clusters[0][:, 1]))
+        # mean 2 line
+        mean_x1 = float(np.mean(line_clusters[1][:, 0]))
+        mean_y1 = float(np.mean(line_clusters[1][:, 1]))
+
+        #middle point
+        mid_x = (mean_x0 + mean_x1) / 2.0
+        mid_y = (mean_y0 + mean_y1) / 2.0
+
+        cv2.circle(resized, (int(mid_x), int(mid_y)), 8, (255, 255, 255), -1) # dot
+
+        l1 = line_clusters[0][np.argsort(line_clusters[0][:, 1])]
+        l2 = line_clusters[1][np.argsort(line_clusters[1][:, 1])]
+
+        map1 = {}
+        map2 = {}
+        #using dictionary in order to exclude values with the same y
+        for x, y in l1:
+            map1.setdefault(y, []).append(x)
+
+        for x, y in l2:
+            map2.setdefault(y, []).append(x)
+
+        common_ys = sorted(set(map1.keys()) & set(map2.keys()))
+
+        mid_points = []
+
+        for y in common_ys[::4]:  # шаг 4 для разрежения
+            x1 = int(np.mean(map1[y]))
+            x2 = int(np.mean(map2[y]))
+
+            mid_x = int((x1 + x2) / 2)
+            mid_points.append((mid_x, y))
+
+        # # smoothed line
+        # window_size = 3
+        # window = np.ones(window_size) / window_size
+        # smoothed_data = np.convolve(mid_points, window, mode='valid')
+        xs = np.array([p[0] for p in mid_points])
+        ys = np.array([p[1] for p in mid_points])
+
+        xs_s = uniform_filter1d(xs, size=7)
+        ys_s = uniform_filter1d(ys, size=7)
+
+        smoothed_data = list(zip(xs_s.astype(int), ys_s.astype(int)))
+        # рисуем middle line
+        for i in range(len(mid_points) - 1):
+            cv2.line(resized, smoothed_data[i], smoothed_data[i + 1], (0, 0, 255), 2)
 
 
-        return norm
-
-    # def counters_preprocess(self, thresh_img):
-    #     hsv_image = cv2.cvtColor(thresh_img, cv2.COLOR_BGR2HSV)
-    #
-    #     # bright colors detection
-    #     lower_yellow = np.array([25, 100, 100])
-    #     upper_yellow = np.array([35, 255, 255])
-    #
-    #     # black and white mask
-    #     black_and_white_mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow)  # make for any bright color
-    #
-    #     return black_and_white_mask
-
-    def detect_counters(self, black_and_white_mask):
-        #finding contours
-        contours, hierarchy = cv2.findContours(black_and_white_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        return contours
-
-    # def draw_counters(self, image, counters):
-    #     for counter in counters:
-    #         if cv2.arcLength(counter, True) > 100:
-    #             cv2.drawContours(image, counter, -1, (0, 255, 0), 2)
-    #     return image
-
-    def auto_canny(self, gray, sigma=0.33):
-        v = np.median(gray)
-        lower = int(max(0, (1.0 - sigma) * v))
-        upper = int(min(255, (1.0 + sigma) * v))
-        return cv2.Canny(gray, lower, upper)
-
-    def line_dection(self, black_and_white_mask):
-        lines = cv2.HoughLinesP(black_and_white_mask, 1, np.pi / 180, threshold=100, minLineLength=50, maxLineGap=5)
-        return lines
-
-    def binary_from_sobel_x(self, gray, ksize=3, thresh=(40, 255)):
-        gx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=ksize)
-        agx = np.absolute(gx)
-        agx = (255 * agx / (agx.max() + 1e-6)).astype(np.uint8)
-        lo, hi = thresh
-        return cv2.inRange(agx, lo, hi)
-
-    def filter_slanted_lines(self, lines, min_angle_deg=25, max_angle_deg=75):
-
-        if lines is None:
-            return None
-
-        filtered = []
-        for l in lines:
-            x1, y1, x2, y2 = l[0]
-            dx = x2 - x1
-            dy = y2 - y1
-            if dx == 0:
-                continue
-
-            angle = abs(np.degrees(np.arctan2(dy, dx)))
-            if angle > 90:
-                angle = 180 - angle
-
-            if min_angle_deg <= angle <= max_angle_deg:
-                filtered.append(l)
-
-        return np.array(filtered) if len(filtered) else None
-
-def apply_outside_lane_roi(mask):
-    h, w = mask.shape[:2]
-    poly = np.array([[
-        (int(0.05*w), h),
-        (int(0.40*w), int(0.45*h)),
-        (int(0.60*w), int(0.45*h)),
-        (int(0.95*w), h),
-    ]], dtype=np.int32)
-
-    roi = np.ones_like(mask) * 255
-    cv2.fillPoly(roi, poly, 0)
-    return cv2.bitwise_and(mask, roi)
-
-def pick_two_best(lines, w, min_angle=25, max_angle=85):
-    if lines is None:
-        return None, None
-
-    bestL, scoreL = None, -1
-    bestR, scoreR = None, -1
-
-    for l in lines:
-        x1, y1, x2, y2 = l[0]
-        dx = x2 - x1
-        dy = y2 - y1
-        if dx == 0:
-            continue
-
-        angle = abs(np.degrees(np.arctan2(dy, dx)))
-        if angle > 90:
-            angle = 180 - angle
-        if not (min_angle <= angle <= max_angle):
-            continue
-
-        slope = dy / dx
-        length = float(np.hypot(dx, dy))
-        x_mid = 0.5 * (x1 + x2)
-
-        score = length
-
-        if slope < 0 and x_mid < w/2:
-            if score > scoreL:
-                bestL, scoreL = l, score
-
-        if slope > 0 and x_mid > w/2:
-            if score > scoreR:
-                bestR, scoreR = l, score
-
-    return bestL, bestR
-
-def extend_simple(line, length=2000):
-    x1, y1, x2, y2 = line[0]
-
-    dx = x2 - x1
-    dy = y2 - y1
-    norm = np.sqrt(dx*dx + dy*dy)
-    if norm == 0:
-        return None
-
-    dx /= norm
-    dy /= norm
-
-    x_start = int(x1 - dx * length)
-    y_start = int(y1 - dy * length)
-    x_end   = int(x2 + dx * length)
-    y_end   = int(y2 + dy * length)
-
-    return (x_start, y_start, x_end, y_end)
+        return resized
 
 def main():
-    line_detect   = LineDetector()
-    image         = cv2.imread("/Users/arsenyzharkoy/PycharmProjects/MrStern/img.png")
-    resized       = line_detect.reseize(image)
-    resized_croped, y = line_detect.region_of_interest(resized)
-    resized_clahe = line_detect.normalization_gray(resized_croped)
+    cap = cv2.VideoCapture("/Users/arsenyzharkoy/PycharmProjects/MrStern/1000010201(1).mp4")
 
-    #blur
-    resized_clahe = cv2.GaussianBlur(resized_clahe, (7, 7), 0)
-    #binary
+    lines_dt = Curved_lines_detector()
 
-    binary = line_detect.binary_from_sobel_x(resized_clahe)
-    binary = apply_outside_lane_roi(binary)
+    while True:
+        ret, frame = cap.read()
 
-    edges = line_detect.auto_canny(resized_clahe)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
-    closed_edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+        if not cap.isOpened():
+            raise RuntimeError('Cannot read from camera')
 
-    lines = line_detect.line_dection(binary)
-    lines = line_detect.filter_slanted_lines(lines)
 
-    h, w = closed_edges.shape[:2]
+        out = lines_dt.frame_preprocessing(frame)
 
-    bestL, bestR = pick_two_best(lines, w)
+        cv2.imshow("frame", out)
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27 or key == ord("q"):
+            break
 
-    midpoints = []
-    black = np.zeros((480, 640, 3), dtype=np.uint8)
-    for l in (bestL, bestR):
-        if l is None:
-            continue
-        l = extend_simple(l)
-        cv2.line(resized, (l[0], l[1]+y), (l[2], l[3]+y), (0, 255, 255), 3)
-
-        x1, y1, x2, y2 = l
-
-        mid_x = (x1 + x2) // 2
-        mid_y = (y1 + y2) // 2
-
-        mid_y += y
-
-        mid_x = int(np.clip(mid_x, 0, resized.shape[1] - 1))
-        mid_y = int(np.clip(mid_y, 0, resized.shape[0] - 1))
-
-        midpoints.append((mid_x, mid_y))
-
-    for center in midpoints:
-        mid_x, mid_y = center
-        cv2.circle(resized, (mid_x, mid_y), 8, (255, 0, 255), -1)
-    try:
-        point1, point2 = midpoints
-        mid_x = (point1[0] + point2[0]) // 2
-        mid_y = (point1[1] + point2[1]) // 2
-
-        x, y = image.shape[:2]
-        cv2.circle(resized, (mid_x, mid_y), 8, (255, 0, 255), -1)
-        cv2.line(resized, (mid_x, 0), (mid_x, y), (255, 0, 255), 3)
-    except:
-        pass
-
-    cv2.imshow("Original_img", binary)
-    cv2.imshow("resized_img", resized)
-    cv2.imshow("edges", edges)
-    cv2.imshow("closed_edges", closed_edges)
-    cv2.waitKey(0)
+    cap.release()
     cv2.destroyAllWindows()
-
-    pass
 
 main()
